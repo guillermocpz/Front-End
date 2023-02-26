3dc4a84faa911b3c5a9b2c587e420aa888390ba9
guillermocpz <73859371+guillermocpz@users.noreply.github.com>
2023-02-26 19:36:03 -0300

    Add files via upload

---
+++ JS/jscript.js
@@ -0,0 +1,12 @@
+
+function Empresa2() {
+    alert('Empresa Nro. 2 - Contacto: Sr. Mario Alvarez - 0283-393829202');
+}
+
+function Empresa3() {
+    alert('Empresa Nro. 3 - Contacto: Ctdor. Juan Perón - 666-840');
+}
+
+function Empresa4() {
+    alert('Empresa Nro. 4 - Contacto: Srta. Susana Torio - 2221-45455555');
+}
